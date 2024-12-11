8bddbd66eebab67d52320aaa885aba25ace82acf
YvesCB <y.bruckmayer@gmail.com>
2024-12-11 08:30:28 +0100

    some more stuff

--- rental_back/src/main/java/ch/juventus/rental/controller/CarController.java
+++ rental_back/src/main/java/ch/juventus/rental/controller/CarController.java
@@ -2,15 +2,8 @@ package ch.juventus.rental.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import ch.juventus.rental.model.Car;
 import ch.juventus.rental.persistance.RentalDatabse;
